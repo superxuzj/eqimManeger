c1df1d8f3916ce9cfb4fe8b52e2135ab988d48c4
superxuzj <271652260@qq.com>
2017-11-09 11:06:33 +0800

    first .

---
+++ src/main/java/com/boliangshenghe/eqim/controller/LoginController.java
@@ -0,0 +1,58 @@
+package com.boliangshenghe.eqim.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.boliangshenghe.eqim.controller.base.BaseCommonController;
+import com.boliangshenghe.eqim.util.CommonUtils;
+
+@Controller
+public class LoginController extends BaseCommonController{
+	
+	
+	@RequestMapping("")
+    public String index(HttpServletRequest request, 
+  			HttpServletResponse response) {
+        return "hello";
+    }
+	
+	@RequestMapping("/index")
+	public String index1(){
+		return "index";
+	}
+	/**
+	 * 首页
+	 * @param request
+	 * @param response
+	 * @return
+	 
+	@RequestMapping("")
+    public String index(HttpServletRequest request, 
+  			HttpServletResponse response) {
+        return "redirect:/login";
+    }*/
+	
+	/**
+	 * 判断一下是否登录
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping("/login")
+    public String login(HttpServletRequest request, 
+  			HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		if(session.getAttribute(CommonUtils.USERID)!=null){
+			return "redirect:/dologin";
+		}else{
+			 return "login";
+		}
+       
+    }
+
+	
+}
---
+++ src/main/java/com/boliangshenghe/eqim/util/CommonUtils.java
@@ -0,0 +1,36 @@
+package com.boliangshenghe.eqim.util;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 静态常量工具类
+ * @author xuzj
+ *
+ */
+public class CommonUtils {
+
+	public static final Integer PAGESIZE = 10;//分页 每页条数
+	
+	public static final String REQUEST_KEY_REQUEST = "_request";// 当前请求
+	
+	public static final String USERID = "sessionuserid";
+	public static final String USERNAME = "sessionusername";
+	public static final String REALNAME = "sessionrealname";
+	public static final String ROLEID = "sessionroleid";
+	public static final String CID = "sessioncid";
+	public static final String COMPANY = "sessioncompany";
+	public static final String ISLOGIN = "islogin";
+	
+	
+	private static final String CACHE_CONTROL = "Cache-Control";
+	private static final String PRAGMA = "Pragma";
+	private static final String HTTP_NO_CACHE = "no-store,no-cache,must-revalidate";
+	private static final String IE_HTTP_NO_CACHE = "post-check=0, pre-check=0";
+	private static final String STANDARD_HTTP_NO_CACHE = "no-cache";
+	 /**no-cache*/
+    public static final void addNoCacheHeader(final HttpServletResponse response){
+		response.setHeader(CACHE_CONTROL, HTTP_NO_CACHE); 
+		response.addHeader(CACHE_CONTROL, IE_HTTP_NO_CACHE); 
+		response.setHeader(PRAGMA, STANDARD_HTTP_NO_CACHE); 
+    }
+}
